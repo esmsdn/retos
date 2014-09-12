282254d051ddcbda881723312ce5a5def8679f91
Alejandro Campos Magencio <alejacma@hotmail.com>
2014-09-12 12:16:29 +0200

    Solución al Reto 1

---
+++ Reto 1 Solucion/Reto1ClassLibrary/Reto1ClassLibrary/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reto1ClassLibrary
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public uint Edad { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            Persona otra = obj as Persona;
+            return (otra != null) && (this.Edad == otra.Edad) && (this.Nombre == otra.Nombre);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Edad.GetHashCode() ^ this.Nombre.GetHashCode();
+        }
+    }
+}
---
+++ Reto 1 Solucion/Reto1ClassLibrary/Reto1ClassLibrary/Reto1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Reto1ClassLibrary
+{
+    public class Reto1
+    {
+        public static List<Persona> OrdenarLista(List<Persona> personas)
+        {
+            if (personas == null)
+            {
+                return null;
+            }
+
+            return personas.OrderByDescending(p => p.Edad).ThenBy(p => p.Nombre).ToList();
+        }
+    }
+}
