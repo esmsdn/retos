457985d90042eb4118ce82ad8047487e8ed4ef92
Alejandro Campos Magencio <alejacma@hotmail.com>
2014-10-03 15:08:41 +0200

    Reto 3

---
+++ Reto 3/ObjectDumper/ObjectDumperTests/Test1Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDumperTests
+{
+    class Test1Class
+    {
+        private string _bar;
+        public string Foo { get; set; }
+        public string Bar { set { _bar = value; }}
+
+        public Test1Class()
+        {
+            Bar = "bar";
+            Foo = "foo";
+        }
+    }
+}
---
+++ Reto 3/ObjectDumper/ObjectDumperTests/Ufo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDumperTests
+{
+    class Ufo
+    {
+        public int Speed { get; set; }
+        public string Name { get; set; }
+        public Planet Origin { get; set; }
+    }
+
+    internal class CrashedUfo : Ufo
+    {
+        public string ZLastProperty
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    class Planet
+    {
+        public string Name { get; set; }
+        public int DaysPerYear { get; set; }
+    }
+}
