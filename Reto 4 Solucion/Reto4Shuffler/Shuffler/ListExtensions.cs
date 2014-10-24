811baec7d88418735c34a95564f60da806b14bc9
Alejandro Campos Magencio <alejacma@hotmail.com>
2014-10-24 12:19:55 +0200

    Solución al Reto 4

---
+++ Reto 4 Solucion/Reto4Shuffler/Shuffler/ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shuffler
+{
+    public static class ListExtensions
+    {
+        private static Random random = new Random();
+
+        public static List<T> Shuffle<T>(this List<T> original)
+        {
+            T[] shuffled = original.ToArray();
+
+            for (int index = shuffled.Length - 1; index > 0; index--)
+            {
+                int randomIndex = random.Next(index);
+
+                T temp = shuffled[index];
+                shuffled[index] = shuffled[randomIndex];
+                shuffled[randomIndex] = temp;
+            }
+
+            return shuffled.ToList();
+        }
+    }
+}
+
+
+
---
+++ Reto 4 Solucion/Reto4Shuffler/UnitTestProject/UnitTestShuffler.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Shuffler;
+using System.Diagnostics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestShuffler
+    {
+        private List<int> GenerateTestList(int count)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+
+        [TestMethod]
+        public void TestShuffle2Items()
+        {
+            List<int> original = new List<int>() { 0, 1 };
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<int> shuffled = original.Shuffle();
+
+            stopwatch.Stop();
+            Console.WriteLine(string.Format("Shuffling 2 items took {0} milliseconds or {1} ticks", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks));
+
+            Assert.AreEqual(original.Count, shuffled.Count);
+            CollectionAssert.AllItemsAreUnique(shuffled);
+            for (int index = 0; index < shuffled.Count; index++)
+            {
+                Assert.AreNotEqual(original[index], shuffled[index]);
+            }
+        }
+
+        [TestMethod]
+        public void TestShuffle3Items()
+        {
+            List<int> original = new List<int>() { 0, 1, 2 };
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<int> shuffled = original.Shuffle();
+
+            stopwatch.Stop();
+            Console.WriteLine(string.Format("{0} {1} {2}", shuffled[0], shuffled[1], shuffled[2]));
+            Console.WriteLine(string.Format("Shuffling 3 items took {0} milliseconds or {1} ticks", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks));
+
+            Assert.AreEqual(original.Count, shuffled.Count);
+            CollectionAssert.AllItemsAreUnique(shuffled);
+            for (int index = 0; index < shuffled.Count; index++)
+            {
+                Assert.AreNotEqual(original[index], shuffled[index]);
+            }
+        }
+
+        [TestMethod]
+        public void TestShuffle10000Items()
+        {
+            List<int> original = GenerateTestList(10000);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<int> shuffled = original.Shuffle();
+
+            stopwatch.Stop();
+            Console.WriteLine(string.Format("Shuffling 10,000 items took {0} milliseconds or {1} ticks", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks));
+
+            Assert.AreEqual(original.Count, shuffled.Count);
+            CollectionAssert.AllItemsAreUnique(shuffled);
+            for (int index = 0; index < shuffled.Count; index++)
+            {
+                Assert.AreNotEqual(original[index], shuffled[index]);
+            }
+        }
+
+        [TestMethod]
+        public void TestShuffle100000Items()
+        {
+            List<int> original = GenerateTestList(100000);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<int> shuffled = original.Shuffle();
+
+            stopwatch.Stop();
+            Console.WriteLine(string.Format("Shuffling 100,000 items took {0} milliseconds or {1} ticks", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks));
+
+            Assert.AreEqual(original.Count, shuffled.Count);
+            CollectionAssert.AllItemsAreUnique(shuffled);
+            for (int index = 0; index < shuffled.Count; index++)
+            {
+                Assert.AreNotEqual(original[index], shuffled[index]);
+            }
+        }
+
+        [TestMethod]
+        public void TestShuffle10000000Items()
+        {
+            List<int> original = GenerateTestList(10000000);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            List<int> shuffled = original.Shuffle();
+
+            stopwatch.Stop();
+            Console.WriteLine(string.Format("Shuffling 10,000,000 items took {0} milliseconds or {1} ticks", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks));
+
+            Assert.AreEqual(original.Count, shuffled.Count);
+            CollectionAssert.AllItemsAreUnique(shuffled);
+            for (int index = 0; index < shuffled.Count; index++)
+            {
+                Assert.AreNotEqual(original[index], shuffled[index]);
+            }
+        }
+    }
+}
