c96f04484e7e730e61706a5dada4a362a4c68dd5
Alejandro Campos Magencio <alejacma@hotmail.com>
2014-10-31 11:28:46 +0100

    Reto 5

---
+++ Reto 5/UnitTestProject1/UnitTestDictionaryPlus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Reto5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestDictionaryPlus
+    {
+        [TestMethod]
+        public void TestDictionaryPlusGetOne()
+        {
+            DictionaryPlus<int, string> dictionary =
+                new DictionaryPlus<int, string>() { { 0, "zero" }, { 1, "one" }, { 2, "two" } };
+            CollectionAssert.AreEqual(new List<string>() { "one" }, dictionary[1].ToList());
+        }
+
+        [TestMethod]
+        public void TestDictionaryPlusGetTwo()
+        {
+            DictionaryPlus<int, string> dictionary =
+                new DictionaryPlus<int, string>() { { 0, "zero" }, { 1, "one" }, { 2, "two" }, { 3, "three" } };
+            CollectionAssert.AreEqual(new List<string>() { "two", "zero" }, dictionary[2, 0].ToList());
+        }
+
+        [TestMethod]
+        public void TestDictionaryPlusGetTen()
+        {
+            DictionaryPlus<char, int> dictionary = new DictionaryPlus<char, int>();
+            for (int i = 0; i < 20; i++)
+            {
+                dictionary.Add((char)('a' + i), i);
+            }
+            CollectionAssert.AreEqual(
+                new List<int>() { 6, 0, 2, 12, 4, 5, 7, 11, 1, 14 },
+                dictionary['g', 'a', 'c', 'm', 'e', 'f', 'h', 'l', 'b', 'o'].ToList());
+        }
+
+        [TestMethod, ExpectedException(typeof(KeyNotFoundException))]
+        public void TestDictionaryPlusGetInvalidKey()
+        {
+            DictionaryPlus<char, int> dictionary = new DictionaryPlus<char, int>();
+            for (int i = 0; i < 13; i++)
+            {
+                dictionary.Add((char)('a' + i), i);
+            }
+            CollectionAssert.AreEqual(
+                new List<int>() { 6, 0, 2, 12, 4, 5, 7, 11, 1, 14 },
+                dictionary['g', 'a', 'c', 'm', 'e', 'f', 'h', 'l', 'b', 'o'].ToList());
+        }
+    }
+}
---
+++ Reto 5/UnitTestProject1/UnitTestDuration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Reto5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestDuration
+    {
+        [TestMethod]
+        public void TestFromDay()
+        {
+            Duration duration = Duration.Day;
+            Assert.AreEqual(new DateTime(1976, 12, 16), duration.From(new DateTime(1976, 12, 15)));
+        }
+
+        [TestMethod]
+        public void TestFromWeek()
+        {
+            Duration duration = Duration.Week;
+            Assert.AreEqual(new DateTime(1976, 12, 16), duration.From(new DateTime(1976, 12, 9)));
+        }
+
+        [TestMethod]
+        public void TestFromMonth()
+        {
+            Duration duration = Duration.Month;
+            Assert.AreEqual(new DateTime(1976, 12, 16), duration.From(new DateTime(1976, 11, 16)));
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestFromInvalidDuration()
+        {
+            Duration duration = (Duration)3;
+            var newDate = duration.From(new DateTime(1976, 12, 16));
+        }
+    }
+}
