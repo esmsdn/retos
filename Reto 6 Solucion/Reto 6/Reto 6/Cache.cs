cdf0fbef11e8fe407ed141e2594a70a70aacb3cb
Alejandro Campos Magencio <alejacma@hotmail.com>
2014-11-20 23:48:02 +0100

    Solución al Reto 6

---
+++ Reto 6 Solucion/Reto 6/Reto 6/Cache.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Reto_6
+{
+    [DebuggerDisplay("Count = {Count}, ActiveCount = {ActiveCount}")]
+    [DebuggerTypeProxy(typeof(CacheDebugView))]
+    public class Cache
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private Dictionary<int, WeakReference> cache = new Dictionary<int, WeakReference>();
+
+        public void Add(int key, object value)
+        {
+            cache.Add(key, new WeakReference(value));
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public int Count
+        {
+            get { return cache.Count; }
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public int ActiveCount
+        {
+            get { return cache.Count(reference => reference.Value.IsAlive); }
+        }
+
+        public object this[int key]
+        {
+            get
+            {
+                return cache[key].Target;
+            }
+        }
+
+        internal class CacheDebugView
+        {
+            private Cache cache;
+
+            public CacheDebugView(Cache cache)
+            {
+                this.cache = cache;
+            }
+
+            [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+            public KeyValuePairs[] Keys
+            {
+                get
+                {
+                    KeyValuePairs[] keys = new KeyValuePairs[cache.Count];
+
+                    int i = 0;
+                    foreach (int key in cache.cache.Keys)
+                    {
+                        keys[i] = new KeyValuePairs(key, cache[key]);
+                        i++;
+                    }
+                    return keys;
+                }
+            }
+        }
+    }
+
+    [DebuggerDisplay("Key = {Key}, Value = {Value}")]
+    internal class KeyValuePairs
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private int Key { get; set; }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private object Value { get; set; }
+
+        public KeyValuePairs(int key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
---
+++ Reto 6 Solucion/Reto 6/Unit Test Reto 6/UnitTestReto6.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reto_6;
+
+namespace Unit_Test_Reto_6
+{
+    [TestClass]
+    public class UnitTestReto6
+    {
+        public class DummyClass
+        {
+            public string Name { get; set; }
+            public byte[] Value { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Dummy {0}", Name);
+            }
+        }
+
+        [TestMethod]
+        public void TestCacheNoItemsAccessedBeforeGC()
+        {
+            Cache cache = new Cache();
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Add(i, i.ToString());
+            }
+
+            Assert.AreEqual(5, cache.Count);
+            Assert.AreEqual(5, cache.ActiveCount);
+            Assert.AreEqual("0", cache[0]);
+            Assert.AreEqual("1", cache[1]);
+            Assert.AreEqual("2", cache[2]);
+            Assert.AreEqual("3", cache[3]);
+            Assert.AreEqual("4", cache[4]);
+
+            GC.Collect();
+
+            Assert.AreEqual(5, cache.Count);
+            Assert.AreEqual(0, cache.ActiveCount);
+            Assert.IsNull(cache[0]);
+            Assert.IsNull(cache[1]);
+            Assert.IsNull(cache[2]);
+            Assert.IsNull(cache[3]);
+            Assert.IsNull(cache[4]);
+        }
+
+        [TestMethod]
+        public void TestCacheSomeItemsAccessedJustBeforeGC()
+        {
+            Cache cache = new Cache();
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Add(i, new byte[1024]);
+            }
+
+            var value1 = cache[1];
+            var value3 = cache[3];
+
+            Assert.AreEqual(5, cache.Count);
+            Assert.AreEqual(5, cache.ActiveCount);
+            Assert.IsNotNull(cache[0]);
+            Assert.IsNotNull(cache[1]);
+            Assert.IsNotNull(cache[2]);
+            Assert.IsNotNull(cache[3]);
+            Assert.IsNotNull(cache[4]);
+
+            GC.Collect();
+
+            Assert.AreEqual(5, cache.Count);
+            Assert.AreEqual(2, cache.ActiveCount);
+            Assert.IsNull(cache[0]);
+            Assert.IsNotNull(cache[1]);
+            Assert.IsNull(cache[2]);
+            Assert.IsNotNull(cache[3]);
+            Assert.IsNull(cache[4]);
+        }
+
+        [TestMethod]
+        public void TestCacheAllItemsAccessedJustBeforeGC()
+        {
+            Cache cache = new Cache();
+            for (int i = 0; i < 4; i++)
+            {
+                cache.Add(i, new DummyClass { Name = i.ToString(), Value = new byte[1024] });
+            }
+
+            var value0 = cache[0];
+            var value1 = cache[1];
+            var value2 = cache[2];
+            var value3 = cache[3];
+
+            Assert.AreEqual(4, cache.Count);
+            Assert.AreEqual(4, cache.ActiveCount);
+            Assert.AreEqual("0", (cache[0] as DummyClass).Name);
+            Assert.AreEqual("1", (cache[1] as DummyClass).Name);
+            Assert.AreEqual("2", (cache[2] as DummyClass).Name);
+            Assert.AreEqual("3", (cache[3] as DummyClass).Name);
+
+            GC.Collect();
+
+            Assert.AreEqual(4, cache.Count);
+            Assert.AreEqual(4, cache.ActiveCount);
+            Assert.AreEqual("0", (cache[0] as DummyClass).Name);
+            Assert.AreEqual("1", (cache[1] as DummyClass).Name);
+            Assert.AreEqual("2", (cache[2] as DummyClass).Name);
+            Assert.AreEqual("3", (cache[3] as DummyClass).Name);
+
+        }
+    }
+}
